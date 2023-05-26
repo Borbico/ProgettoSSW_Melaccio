e7b43f4bf221aa2b0a0698f7a8a6499b03dea931
Borbico <126683530+Borbico@users.noreply.github.com>
2023-05-26 11:16:32 +0200

    3 files modified

--- src/root/acquisizione/acquisizione.component.ts
+++ src/root/acquisizione/acquisizione.component.ts
@@ -1,14 +1,46 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AjaxResponse } from 'rxjs/ajax';
+import { infolibro } from '../infolibro';
+import { Archivio } from '../archivio';
+import { ArchivioService } from '../archivio.service';
 
 @Component({
   selector: 'app-acquisizione',
   templateUrl: './acquisizione.component.html',
   styleUrls: ['./acquisizione.component.css'],
   standalone: true,
+  imports: [CommonModule],
 })
 export class AcquisizioneComponent implements OnInit {
-  constructor() {}
+  @Input() acquisizione: number;
+  @Input() archivioAttuale: Archivio;
+
+  setAcquisizione(valore: number) {
+    this.acquisizione = valore;
+  }
+
+  acquisisciLibro() {
+    var titolo: HTMLInputElement = document.getElementById(
+      'titolo'
+    ) as HTMLInputElement;
+    var autore: HTMLInputElement = document.getElementById(
+      'autore'
+    ) as HTMLInputElement;
+    var posizione: HTMLInputElement = document.getElementById(
+      'posizione'
+    ) as HTMLInputElement;
+    const nuovoLibro = new infolibro(
+      titolo.value,
+      autore.value,
+      posizione.value,
+      'undefined'
+    );
+    this.archivioAttuale.acquisisciLibro(nuovoLibro);
+    this.setAcquisizione(0);
+  }
+
+  constructor(private ar: ArchivioService) {}
 
   ngOnInit() {}
 }
--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { RicercaComponent } from './ricerca/ricerca.component';
 import { AcquisizioneComponent } from './acquisizione/acquisizione.component';
+import { Archivio } from './archivio';
+import { ArchivioService } from './archivio.service';
 import { CommonModule } from '@angular/common';
+import { AjaxResponse } from 'rxjs/ajax';
 
 
 @Component({
@@ -13,9 +16,23 @@ import { CommonModule } from '@angular/common';
 })
   
 export class RootComponent implements OnInit {
+  acquisizione: number = 0;
+  archivioAttuale = new Archivio(this.ar);
+
+  setAcquisizione(valore: number) {
+    this.acquisizione = valore;
+  }
+
+  costruisciArchivio() {
+    this.ar.getArchivio().subscribe({
+      next: (data: AjaxResponse<any>) => console.log(data.response),
+      error: (e) => console.error('Errore di acquisizione dell\'archivio: ' + JSON.stringify(e))
+    });
+
+  }
   title: string = 'Servizio di biblioteca online';
 
-  constructor() {}
+  constructor(private ar: ArchivioService) {}
 
   ngOnInit() {}
 }
