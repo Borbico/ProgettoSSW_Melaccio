1c9a772ba8a38bf94e2ece48a5bc113567523c22
Borbico <126683530+Borbico@users.noreply.github.com>
2023-05-31 11:45:03 +0200

    3 files modified

--- src/root/ricerca/prestito/prestito.component.ts
+++ src/root/ricerca/prestito/prestito.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AjaxResponse } from 'rxjs/ajax';
+import { infolibro } from '../../infolibro';
+import { Archivio } from '../../archivio';
+import { ArchivioService } from '../../archivio.service';
 
 @Component({
   selector: 'app-prestito',
   templateUrl: './prestito.component.html',
   styleUrls: ['./prestito.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule]
 })
-export class PrestitoComponent implements OnInit {
 
-  constructor() { }
+export class PrestitoComponent implements OnInit {
+  @Input() libroSelezionato: infolibro;
+  @Input() archivioAttuale: Archivio = new Archivio(this.ar)
+
+  prestaLibro() {
+    var nomePrestatario: HTMLInputElement = document.getElementById('prestatario') as HTMLInputElement;
+    this.archivioAttuale.prestitoLibro(this.libroSelezionato, nomePrestatario.value)
+  }
+
+  restituisciLibro() {
+    this.archivioAttuale.prestitoLibro(this.libroSelezionato, 'undefined');
+  }
+
+  constructor(private ar: ArchivioService) {}
 
   ngOnInit() {
   }
